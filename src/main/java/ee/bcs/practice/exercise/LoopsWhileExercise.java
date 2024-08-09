d164f165787ca9727bc225d78a5ad7231fc1e0ad
MeelisTe <meelis.teern@gmail.com>
2024-08-09 22:34:47 +0300

    SD_07_P1_S2_025 - loops(while) exercises

---
+++ src/main/java/ee/bcs/practice/exercise/LoopsWhileExercise.java
@@ -0,0 +1,19 @@
+package ee.bcs.practice.exercise;
+
+public class LoopsWhileExercise {
+
+    public static void main(String[] args) {
+        // Exercise 1: Simple While Loop
+
+
+        // Exercise 2: Calculate Sum Using While Loop
+
+
+        // Exercise 3: Breaking a While Loop
+
+
+        // Exercise 4: Do/While Loop
+
+
+    }
+}
