8070aebc3cb84d35f9bc8b1f156b601901877f80
MeelisTe <meelis.teern@gmail.com>
2024-08-11 23:07:31 +0300

    SD_07_P1_S2_026 For loop with index exercise

---
+++ src/main/java/ee/bcs/practice/exercise/ForLoopWithIndexExercise.java
@@ -0,0 +1,22 @@
+package ee.bcs.practice.exercise;
+
+public class ForLoopWithIndexExercise {
+
+    public static void main(String[] args) {
+        // Exercise 1: Print Numbers from 1 to 5
+
+
+        // Exercise 2: Print Even Numbers from 0 to 10
+
+
+        // Exercise 3: Print Numbers from 5 to 1
+
+
+        // Exercise 4: Nested Loop to Print a 3x3 Grid
+
+
+        // Exercise 5: Iterating Over an Array
+
+
+    }
+}
