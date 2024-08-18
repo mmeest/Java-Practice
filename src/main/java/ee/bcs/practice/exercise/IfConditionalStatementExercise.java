9c6847b9a2a49bd5bf52b5f02a051f3dc0a01411
MeelisTe <meelis.teern@gmail.com>
2024-08-18 15:54:03 +0300

    SD_08_P1_S3_010 - if conditional statement exercises

---
+++ src/main/java/ee/bcs/practice/exercise/IfConditionalStatementExercise.java
@@ -0,0 +1,16 @@
+package ee.bcs.practice.exercise;
+
+public class IfConditionalStatementExercise {
+    public static void main(String[] args) {
+
+        // Exercise 1: Check if a number is positive
+
+
+        // Exercise 2: Check if a number is equal to 10
+
+
+        // Exercise 3: Check if a person is eligible to vote
+
+
+    }
+}
