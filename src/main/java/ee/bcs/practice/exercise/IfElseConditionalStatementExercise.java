09b34c8b78fa8ed3a7a7a82a5c057411fecdbe62
MeelisTe <meelis.teern@gmail.com>
2024-08-20 15:56:11 +0300

    SD_08_P1_S3_011 - if else conditional statement exercises

---
+++ src/main/java/ee/bcs/practice/exercise/IfElseConditionalStatementExercise.java
@@ -0,0 +1,16 @@
+package ee.bcs.practice.exercise;
+
+public class IfElseConditionalStatementExercise {
+
+    public static void main(String[] args) {
+        // Exercise 1: Check if a number is positive or negative
+
+
+        // Exercise 2: Check if a number is equal to 10
+
+
+        // Exercise 3: Check if a person is a minor or an adult
+
+
+    }
+}
