08a3faceaf279159e518a8aaaed92f14f94a4a5b
MeelisTe <meelis.teern@gmail.com>
2024-08-20 16:18:24 +0300

    SD_08_P1_S3_012 - if else if conditional statement exercises

---
+++ src/main/java/ee/bcs/practice/exercise/IfElseIfConditionalStatementExercise.java
@@ -0,0 +1,16 @@
+package ee.bcs.practice.exercise;
+
+public class IfElseIfConditionalStatementExercise {
+
+    public static void main(String[] args) {
+        // Exercise 1: Check if a number is positive, negative, or zero
+
+
+        // Exercise 2: Determine the grade category of a student
+
+
+        // Exercise 3: Check the temperature range
+
+
+    }
+}
