789dd1f11c4664352805113bd12ac444d82b81a3
MeelisTe <meelis.teern@gmail.com>
2024-08-22 21:40:17 +0300

    SD_08_P1_S3_014 - logical operators exercises

---
+++ src/main/java/ee/bcs/practice/exercise/LogicalOperatorsExercise.java
@@ -0,0 +1,19 @@
+package ee.bcs.practice.exercise;
+
+public class LogicalOperatorsExercise {
+
+    public static void main(String[] args) {
+        // Exercise 1: Implement the AND Operation
+
+
+        // Exercise 2: Implement the OR Operation
+
+
+        // Exercise 3: Implement the NOT Operation
+
+
+        // Exercise 4: Combine Logical Operators for Access Control
+
+
+    }
+}
