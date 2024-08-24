fd224b2f3d72fd9b7f700053eee0fda3875feb06
MeelisTe <meelis.teern@gmail.com>
2024-08-24 15:20:46 +0300

    SD_09_P1_S4_006 - format

--- src/main/java/ee/bcs/practice/exercise/MethodsInTheSameClassExercise.java
+++ src/main/java/ee/bcs/practice/exercise/MethodsInTheSameClassExercise.java
@@ -5,6 +5,7 @@ public class MethodsInTheSameClassExercise {
     public static void main(String[] args) {
         // Exercise 1 Call the getMessage method and print the result.
 
+
         // Exercise 2 Call the multiplyNumbers method and print the result.
 
 
