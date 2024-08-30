10ecc947f1b16b74915bc20ba07910b11b5a900f
MeelisTe <meelis.teern@gmail.com>
2024-08-30 11:04:35 +0300

    SD_08_P1_S3_006 - increment and decrement operators exercise

---
+++ src/main/java/ee/bcs/practice/exercise/IncrementAndDecrementOperatorsExercise.java
@@ -0,0 +1,19 @@
+package ee.bcs.practice.exercise;
+
+public class IncrementAndDecrementOperatorsExercise {
+
+    public static void main(String[] args) {
+        // Exercise 1: Pre-Increment
+
+
+        // Exercise 2: Post-Increment
+
+
+        // Exercise 3: Pre-Decrement
+
+
+        // Exercise 4: Post-Decrement
+
+
+    }
+}
