9b820d4e1b9adc3f48cae150de66d835b844e36a
MeelisTe <meelis.teern@gmail.com>
2024-09-14 11:25:15 +0300

    SD_07_P1_S2_013 - LocalVariablesExercise

---
+++ src/main/java/ee/bcs/practice/exercise/LocalVariablesExercise.java
@@ -0,0 +1,16 @@
+package ee.bcs.practice.exercise;
+
+public class LocalVariablesExercise {
+
+    // Exercise 1: Local Variables in Methods
+
+    // Exercise 2: Local Variable Scope
+
+    public static void main(String[] args) {
+
+        // Run Exercise 1
+
+        // Run Exercise 2
+
+    }
+}
