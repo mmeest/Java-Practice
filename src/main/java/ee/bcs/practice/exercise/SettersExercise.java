6fc62b098e6995c305ae7b7a2d4d198f22af4507
MeelisTe <meelis.teern@gmail.com>
2024-09-27 13:03:29 +0300

    SD_11_P1_S5_009 SettersExercise

---
+++ src/main/java/ee/bcs/practice/exercise/SettersExercise.java
@@ -0,0 +1,17 @@
+package ee.bcs.practice.exercise;
+
+public class SettersExercise {
+
+    public static void main(String[] args) {
+
+        // Exercise 1: Implement a Person class with a setter for the name
+
+
+        // Exercise 2: Implement a validation in the setter for the age
+
+
+        // Exercise 3: Use a getter to retrieve and print the name and age of the person
+
+
+    }
+}
