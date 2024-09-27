6ddf9348e3cbe725fd068f59623302f80ab8963c
MeelisTe <meelis.teern@gmail.com>
2024-09-27 13:15:57 +0300

    SD_11_P1_S5_009 SettersExercise person changed to user

--- src/main/java/ee/bcs/practice/exercise/SettersExercise.java
+++ src/main/java/ee/bcs/practice/exercise/SettersExercise.java
@@ -4,13 +4,13 @@ public class SettersExercise {
 
     public static void main(String[] args) {
 
-        // Exercise 1: Implement a Person class with a setter for the name
+        // Exercise 1: Implement a User class with a setter for the name
 
 
         // Exercise 2: Implement a validation in the setter for the age
 
 
-        // Exercise 3: Use a getter to retrieve and print the name and age of the person
+        // Exercise 3: Use a getter to retrieve and print the name and age of the user
 
 
     }
