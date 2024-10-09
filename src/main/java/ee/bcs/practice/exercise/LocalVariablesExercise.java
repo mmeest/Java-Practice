f90f6ece4594bfdec5cbd2291ed33329fc33654c
Rain Tüür <rain.tyyr@bcs.ee>
2024-10-09 09:49:37 +0300

    updated LocalVariablesExercise start template

--- src/main/java/ee/bcs/practice/exercise/LocalVariablesExercise.java
+++ src/main/java/ee/bcs/practice/exercise/LocalVariablesExercise.java
@@ -2,12 +2,13 @@ package ee.bcs.practice.exercise;
 
 public class LocalVariablesExercise {
 
-
-
     public static void main(String[] args) {
+
         // Exercise 1:
 
         // Exercise 2:
+
+        // Exercise 3:
 
         // Run main method
 
