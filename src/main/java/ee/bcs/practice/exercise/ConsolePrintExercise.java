8ba642bd525b9808d7211b133c7914612f53d0bb
MeelisTe <meelis.teern@gmail.com>
2024-10-16 00:24:11 +0300

    ConsolePrintExercise added

---
+++ src/main/java/ee/bcs/practice/exercise/ConsolePrintExercise.java
@@ -0,0 +1,18 @@
+package ee.bcs.practice.exercise;
+
+public class ConsolePrintExercise {
+
+    public static void main(String[] args) {
+
+        // Example 1: Using System.out.print()
+
+
+        System.out.println(); // Print a new line for clarity in output
+
+        // Example 2: Using System.out.println()
+
+
+        // Run main method
+
+    }
+}
