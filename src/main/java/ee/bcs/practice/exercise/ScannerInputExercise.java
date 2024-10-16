cafe964e279ed383f381a03bac45e97656794139
MeelisTe <meelis.teern@gmail.com>
2024-10-17 00:24:04 +0300

    ScannerInputExercise

---
+++ src/main/java/ee/bcs/practice/exercise/ScannerInputExercise.java
@@ -0,0 +1,20 @@
+package ee.bcs.practice.exercise;
+
+import java.util.Scanner;
+
+public class ScannerInputExercise {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Exercise 1: First and Last Name Input
+
+
+        // Exercise 2: Choose a number
+
+
+        // Run main method
+
+    }
+}
