9ff585149cd6ab39f5bbcef5e5a538f259785342
Rain Tüür <rain.tyyr@bcs.ee>
2024-11-08 09:09:29 +0200

    updated IncrementAndDecrementOperatorsExercise

--- src/main/java/ee/bcs/practice/exercise/IncrementAndDecrementOperatorsExercise.java
+++ src/main/java/ee/bcs/practice/exercise/IncrementAndDecrementOperatorsExercise.java
@@ -4,16 +4,13 @@ public class IncrementAndDecrementOperatorsExercise {
 
     public static void main(String[] args) {
 
-        // Exercise 1: Pre-Increment
+        // Exercise 1: Implement Pre-Increment Operation
 
+        // Exercise 2: Implement Post-Increment Operation
 
-        // Exercise 2: Post-Increment
+        // Exercise 3: Implement Pre-Decrement Operation
 
-
-        // Exercise 3: Pre-Decrement
-
-
-        // Exercise 4: Post-Decrement
+        // Exercise 4: Implement Post-Decrement Operation
 
 
         // Run main method
