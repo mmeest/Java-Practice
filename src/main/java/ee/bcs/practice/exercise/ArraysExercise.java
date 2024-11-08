6a6c917fc9fa9643fb110f9dfbf9dd7e5503dfcd
Rain Tüür <rain.tyyr@bcs.ee>
2024-11-08 08:58:53 +0200

    updated ArraysExercise

--- src/main/java/ee/bcs/practice/exercise/ArraysExercise.java
+++ src/main/java/ee/bcs/practice/exercise/ArraysExercise.java
@@ -6,15 +6,13 @@ public class ArraysExercise {
 
         // Exercise 1: Create and Print Array
 
-
         // Exercise 2: Array Length
-
 
         // Exercise 3: Access Array Elements
 
-
         // Exercise 4: Two-Dimensional Array
 
+        // Exercise 5: Choose Your Favorite Fruit
 
         // Run main method
 
